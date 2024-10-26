a3d1c10f54ac50fe9ea34615849c9cbd8f2bc3b2
SUTAM PAUL <sutampaul990@gmail.com>
2024-10-26 21:14:00 +0530

    updated parameterized constructor

--- parameterized_constructor.java
+++ parameterized_constructor.java
@@ -5,6 +5,12 @@ class Test{
         a = x;
         b = y;
     }
+    Test(int a,String b){
+        System.out.println(a + " " +b);
+    }
+    Test(char ch){
+        System.out.println(ch);
+    }
     void show(){
         System.out.println(a+ " " + b);
     }
@@ -12,7 +18,9 @@ class Test{
 
 public class parameterized_constructor {
     public static void main(String[] args) {
-        Test t = new Test(100,200);
-        t.show();
+        Test t1 = new Test(100,200);
+        Test t2 = new Test(300,"Sutam");
+        Test t3 = new Test('c');
+        t1.show();
     }
 }
